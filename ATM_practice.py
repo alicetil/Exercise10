efd1179cc0d1ed628770302f008f780e2d2847d8
alice <alice.tildsley@gmail.com>
2022-02-16 19:22:46 +0000

    took out some comments

--- ATM_practice.py
+++ ATM_practice.py
@@ -16,26 +16,3 @@ while tries < 3:
     tries =+ 1
     if max_tries == tries:
         print("You have reached your max number of tries.")
-
-
-    # print("You have had", tries, "attempts of", max_tries)
-
-    # if tries = 0:
-    # print("No attempts left")
-
-# if supplied_pin == pin:
-#     print("PIN entry is successful")
-# else:
-#     print("PIN incorrect")
-#     if tries > 0 and tries <= 3:
-#         tries = tries - 1
-#         print("You have", tries, "attempts remaining")
-#         supplied_pin = (input("Please try again: "))
-#
-# if supplied_pin == pin:
-#     print("PIN entry is successful")
-# else:
-#     print("PIN incorrect")
-#     if tries > 0 and tries <= 3:
-#         tries = tries - 1
-#         print("You have", tries, "attempts remaining")
